c2aa2f233da8b0dc8bac6560cb43e238d76179ea
nipunagarwal9636 <58950264+nipunagarwal9636@users.noreply.github.com>
2026-01-17 19:42:49 +0530

    Update API key retrieval method in app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ if process_url_clicked:
 
 # ---------------- LLM ----------------
 llm = ChatGroq(
-    groq_api_key=GROQ_API_KEY,
+    groq_api_key=os.environ["GROQ_API_KEY"],
     model_name="llama-3.3-70b-versatile",
     temperature=0
 )
